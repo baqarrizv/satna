eb2f73e4a8dbe6813e9481c0dff8564aa5f682ca
baqararwaj <baqar.arwaj@gmail.com>
2025-04-09 15:02:19 +0500

    Redirect issue show alert

--- resources/views/payments/redirect.blade.php
+++ resources/views/payments/redirect.blade.php
@@ -2,15 +2,24 @@
 <html>
 <head>
     <title>Redirecting...</title>
-    <script>
-        // Open invoice in new tab
-        window.open("{{ $invoice_url }}", "_blank");
-        
-        // Redirect current page
-        window.location.href = "{{ $redirect_url }}";
-    </script>
 </head>
 <body>
     <p>Redirecting, please wait...</p>
+
+    <script>
+        // Wait for the page to load and then simulate user interaction to avoid pop-up blockers
+       
+            // Open invoice in a new tab
+            var newTab = window.open("{{ $invoice_url }}", "_blank");
+
+            if (newTab) {
+                // If the tab opened successfully (not blocked), redirect the current page.
+                window.location.href = "{{ $redirect_url }}";
+            } else {
+                // If the pop-up was blocked, alert the user
+                alert("Please allow pop-ups for this site to view the invoice.");
+            }
+       
+    </script>
 </body>
-</html> 
+</html>
