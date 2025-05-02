1394be8f5623ba16bf3cbf429322902ba3f3018f
baqarrizv <baqarrizvi72@gmail.com>
2025-05-02 11:07:16 +0000

    add 403 page

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,35 @@
+
+@extends('layouts.master-without-nav')
+@section('title')
+    Error 403
+@endsection
+@section('content')
+    <div class="my-5 pt-sm-5">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="text-center">
+                        <div>
+                            <div class="row justify-content-center">
+                                <div class="col-sm-4">
+                                    <div class="error-img">
+                                        <img src="{{ URL::asset('/assets/images/403-error.png') }}" alt=""
+                                            class="img-fluid mx-auto d-block">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <h4 class="text-uppercase mt-4">Module Restricted</h4>
+                        <p class="text-muted">This action is unauthorized.</p>
+                        <div class="mt-5">
+                            <a class="btn btn-primary waves-effect waves-light" href="{{ url('/') }}">Back to
+                                Dashboard</a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
